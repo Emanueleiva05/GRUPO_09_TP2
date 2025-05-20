961b098485044cea6b4600e7f9052c2551b7ad4c
Tomas Battistella <tomas.battistella0@gmail.com>
2025-05-20 12:25:20 -0300

    Se modifico el front de la pagina principal

--- app/src/main/java/com/example/proyecto/ui/screens/CiudadesScreen.kt
+++ app/src/main/java/com/example/proyecto/ui/screens/CiudadesScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import android.content.Context
 import android.widget.Button
+import androidx.compose.foundation.border
 import androidx.compose.runtime.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -25,20 +26,87 @@ import java.nio.file.WatchEvent
 @Composable
 fun CiudadesScreen(navController: NavHostController) {
     var paisNom by remember { mutableStateOf("") }
+    var ciudadNom by remember { mutableStateOf("") }
     val context = LocalContext.current
     val dbHelper = DBHelper(context)
 
-    Column(modifier = Modifier.fillMaxSize()
-        .padding(20.dp)) {
-
-        Box {
-            Row { //Para crear el pais
+    Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column {
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                //Para crear el pais
                 Button(onClick = { navController.navigate("crear_pais") },
-                        modifier = Modifier.height(50.dp)
-                            .width(127.dp)) {
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(127.dp)) {
                     Text("Crear pais")
                 }
-                Spacer(Modifier.width(20.dp))
+                //Para crear una ciudad
+                Button(onClick = {navController.navigate("crear_ciudad")},
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(127.dp)) {
+                    Text("Crear ciudad")
+                }
+            }
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp)
+                ) {
+                TextField(
+                    value = ciudadNom,
+                    onValueChange = { nuevoTexto ->
+                        ciudadNom = nuevoTexto
+                        },
+                        label = { Text("Ingrese el nombre de una ciudad") },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp),
+                        shape = RoundedCornerShape(24.dp),
+                        colors = TextFieldDefaults.colors(
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent
+                        )
+                    )
+            }
+        }
+
+        Box ( //Box que simula la futura tabla de busqueda
+            modifier = Modifier
+                .weight(1f) // Ocupa el espacio disponible entre lo de arriba y lo de abajo
+                .fillMaxWidth() // Que ocupe todo el ancho disponible
+                .border(2.dp, Color.Gray) // Borde visible para referencia
+                .padding(9.dp)
+        ) {
+            Text("Aca tendria que estar la tabla de ciudades")
+        }
+
+        Column (modifier = Modifier.padding(vertical = 16.dp)) {
+            Row { //Para borrar ciudades de un pais
+                Button(
+                    onClick = { dbHelper.borrarCiudadesDeUnPais(paisNom) },
+                    modifier = Modifier
+                        .height(53.dp)
+                        .width(127.dp)
+                ) {
+                    Text(
+                        text = "Borrar ciudades",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(10.dp))
+
                 TextField(
                     value = paisNom,
                     onValueChange = { nuevoTexto ->
@@ -55,58 +123,23 @@ fun CiudadesScreen(navController: NavHostController) {
                     )
                 )
             }
-        }
 
-        Spacer(modifier = Modifier.height(10.dp))
+            Spacer(modifier = Modifier.height(20.dp))
 
-        //Para crear una ciudad
-        Button(onClick = {navController.navigate("crear_ciudad")},
+            Row (
                 modifier = Modifier
-                    .height(50.dp)) {
-            Text("Crear ciudad")
-        }
-
-        Spacer(modifier = Modifier.height(500.dp))
-
-        Row { //Para borrar ciudades de un pais
-            Button(onClick = { dbHelper.borrarCiudadesDeUnPais(paisNom) },
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                //Para modificar la poblacion de una ciudad
+                Button(
+                    onClick = {navController.navigate("modificar_ciudad")},
                     modifier = Modifier
-                        .height(53.dp)
-                        .width(127.dp)
-                    ){
-                Text(
-                    text = "Borrar ciudades",
-                    modifier = Modifier.fillMaxWidth(),
-                    textAlign = TextAlign.Center
-                    )
+                        .height(53.dp),
+                ) {
+                    Text("Modificar poblacion de ciudad ")
+                }
             }
-
-            Spacer(modifier = Modifier.width(10.dp))
-
-            TextField(
-                value = paisNom,
-                onValueChange = { nuevoTexto ->
-                    paisNom = nuevoTexto
-                },
-                label = { Text("Nombre del pais") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(50.dp),
-                shape = RoundedCornerShape(24.dp),
-                colors = TextFieldDefaults.colors(
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent
-                )
-            )
-        }
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        //Para modificar la poblacion de una ciudad
-        Button(onClick = {navController.navigate("modificar_ciudad")},
-                modifier = Modifier
-                    .height(53.dp)) {
-            Text("Modificar poblacion de ciudad ")
         }
     }
 }
